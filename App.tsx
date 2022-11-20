d88106072b8d1b2809cf508443189ba75bf45cef
shu-illy <sh.illy.9117@gmail.com>
2022-11-20 14:13:50 +0900

    Lesson 63

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ const App = () => {
             <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
           </View>
           <View>
-            <Text>X</Text>
+            <Text>x</Text>
           </View>
         </View>
       </View>
@@ -30,7 +30,7 @@ const App = () => {
             <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
           </View>
           <View>
-            <Text>X</Text>
+            <Text>x</Text>
           </View>
         </View>
       </View>
@@ -42,13 +42,13 @@ const App = () => {
             <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
           </View>
           <View>
-            <Text>X</Text>
+            <Text>x</Text>
           </View>
         </View>
       </View>
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   );
@@ -99,6 +99,27 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: '#848484'
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#000000', // iOSにしか適用できないプロパティ
+    shadowOffset: { width: 0, height: 8 }, // iOSにしか適用できないプロパティ
+    shadowOpacity: 0.25, // iOSにしか適用できないプロパティ
+    shadowRadius: 8, // iOSにしか適用できないプロパティ
+    elevation: 8, // Androidにしか適用できないプロパティ
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
   }
 });
 
