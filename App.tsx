77b83de1d134b1ef911444f16cfae30492bc0f4d
shu-illy <sh.illy.9117@gmail.com>
2022-11-25 02:03:44 +0900

    Lesson 77

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { MemoCreateScreen } from "./src/screens/MemoCreateScreen";
+// import { LogInScreen } from "./src/screens/LogInScreen";
+import { SignUpScreen } from "./src/screens/SineUpScreen";
+// import { MemoCreateScreen } from "./src/screens/MemoCreateScreen";
 // import { MemoEditScreen } from "./src/screens/MemoEditScreen";
 // import { MemoDetailScreen } from "./src/screens/MemoDetailScreen";
 // import { MemoListScreen } from "./src/screens/MemoListScreen";
@@ -8,7 +10,9 @@ const App = () => {
   // return <MemoListScreen />;
   // return <MemoDetailScreen />;
   // return <MemoEditScreen />;
-  return <MemoCreateScreen />;
+  // return <MemoCreateScreen />;
+  // return <LogInScreen />;
+  return <SignUpScreen />;
 };
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+type Props = {
+  label: string;
+};
+
+export const Button: FC<Props> = ({ label }) => {
+  return (
+    <View style={styles.buttonContainer}>
+      <Text style={styles.buttonLabel}>{label}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0F4F8",
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    borderColor: "#DDDDDD",
+    borderWidth: 1,
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: "#467FD3",
+    borderRadius: 4,
+    alignSelf: "flex-start",
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: "#FFFFFF",
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: "#467FD3",
+  },
+  footer: {
+    flexDirection: "row",
+  },
+});
