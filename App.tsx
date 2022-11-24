25d7cba630250683e27e68cad618908c2b1c0c36
shu-illy <sh.illy.9117@gmail.com>
2022-11-25 01:31:56 +0900

    Lesson 73

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { MemoEditScreen } from "./src/screens/MemoEditScreen";
+import { MemoCreateScreen } from "./src/screens/MemoCreateScreen";
+// import { MemoEditScreen } from "./src/screens/MemoEditScreen";
 // import { MemoDetailScreen } from "./src/screens/MemoDetailScreen";
 // import { MemoListScreen } from "./src/screens/MemoListScreen";
 
 const App = () => {
   // return <MemoListScreen />;
   // return <MemoDetailScreen />;
-  return <MemoEditScreen />;
+  // return <MemoEditScreen />;
+  return <MemoCreateScreen />;
 };
 
 export default App;
